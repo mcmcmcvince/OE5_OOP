71f72e806d8b3af50a7a8d4c0d968820171298d5
mcmcmcvince <91184245+mcmcmcvince@users.noreply.github.com>
2022-06-23 23:00:38 +0800

    Add files via upload

--- EmployeeDriver.java
+++ EmployeeDriver.java
@@ -3,7 +3,7 @@ public class EmployeeDriver {
 
     public static void main(String[] args) {
 
-        AccountGenerator emp = new AccountGenerator();
+        Employee emp = new Employee();
         System.out.println(emp);
     }
 
